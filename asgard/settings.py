8dd6d733f230eda46b0c452f30b5e65bcae88a0a
uzaxirr <uzaxirr@gmail.com>
2023-09-18 15:07:07 +0530

    CORS origin allow

--- asgard/settings.py
+++ asgard/settings.py
@@ -58,6 +58,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",  # CORS Middleware
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -65,13 +66,12 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",  # CORS Middleware
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_ALL_HEADERS = True
-CORS_ALLOW_METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
+CORS_ALLOW_CREDENTIALS = False
+
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3001", "http://127.0.0.1:3000"]
 
 
 ROOT_URLCONF = "asgard.urls"
