3b07cdcda5ddde90f06ac19d99edb95ec3c09c41
uzaxirr <uzaxirr@gmail.com>
2023-09-19 22:04:02 +0530

    CORS Allow all origin

--- asgard/settings.py
+++ asgard/settings.py
@@ -71,13 +71,7 @@ MIDDLEWARE = [
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = False
 
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:3001",
-    "http://127.0.0.1:3000",
-    "http://localhost",
-    "http://localhost:3000",
-    "http://localhost:3001",
-]
+# CORS_ALLOWED_ORIGINS = ["*"]
 
 
 ROOT_URLCONF = "asgard.urls"
